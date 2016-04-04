2bd7a1d1106ed8241ba796815b93124172e142f8
Danilo Bargen <mail@dbrgn.ch>
2016-04-04 22:23:27 +0200

    Finish main implementation

--- logger.py
+++ logger.py
@@ -1,21 +1,56 @@
 #!/usr/bin/env python3
 
+import sys
+import os
+
 from influxdb import InfluxDBClient
+import requests
 
-DB_USER = 'root'
-DB_PASS = 'root'
-DB_NAME = 'spaceapi'
 
-client = InfluxDBClient('localhost', 8086, DB_USER, DB_PASS, DB_NAME)
+env = os.environ.get
 
-# Create database if necessary
-if DB_NAME not in (d['name'] for d in client.get_list_database()):
-    client.create_database(DB_NAME)
 
-# Write a random datapoint
-client.write_points([{
-    'measurement': 'people_now_present',
-    'fields': {
-        'value': 42,
-    },
-}])
+DB_HOST = env('INFLUXDB_HOST', 'localhost')
+DB_PORT = int(env('INFLUXDB_PORT', '8086'))
+DB_USER = env('INFLUXDB_USER', 'root')
+DB_PASS = env('INFLUXDB_PASS', 'root')
+DB_NAME = env('INFLUXDB_DB', 'spaceapi')
+STATUS_URL = env('STATUS_URL', 'https://status.crdmp.ch/')
+
+
+def main():
+    # Influx client
+    client = InfluxDBClient(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME)
+
+    # Create database if necessary
+    if DB_NAME not in (d['name'] for d in client.get_list_database()):
+        client.create_database(DB_NAME)
+
+    # Request status response
+    resp = requests.get(STATUS_URL)
+    resp.raise_for_status()
+
+    # Parse data
+    data = resp.json()
+
+    # Build datapoints
+    datapoints = []
+    for sensor in data['sensors']['people_now_present']:
+        datapoints.append({
+            'measurement': 'people_present',
+            'fields': sensor,
+        })
+    for sensor in data['sensors']['temperature']:
+        datapoints.append({
+            'measurement': 'temperature',
+            'fields': sensor,
+        })
+
+    # Write datapoints
+    if not client.write_points(datapoints):
+        print('Error: Could not write datapoints!')
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
